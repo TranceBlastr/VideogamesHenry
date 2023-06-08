1b949fc8d88edfbc4aa05626a753a8a2bf3ed706
tranceblastr <brian.andrais@gmail.com>
2023-06-07 22:50:33 -0300

    Primer commit

---
+++ api/src/models/Genres.js
@@ -0,0 +1,24 @@
+// const { timeStamp } = require('console');
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Genres",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
---
+++ api/src/models/Videogame.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: "Game description.",
+      },
+      platform: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        defaultValue: "No platform especified.",
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue: "No image available",
+      },
+      launch: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
