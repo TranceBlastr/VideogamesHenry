12e7f2f1cddb982288fc26e3daf64098ba498441
tranceblastr <brian.andrais@gmail.com>
2023-06-09 13:07:36 -0300

    Rutas optimizadas, gameHandler, controllers definidos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,18 +1,18 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const gameHandler = require("../handlers/gameHandler");
+const gamePostHandler = require("../handlers/gamepostHandler");
+const genreHandler = require("../handlers/genreHandler");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get("/videogames", videogameHandler);
+router.get("/videogames", gameHandler);
 
-router.get("/videogames/:id", videogameHandlerById);
-
-router.get("/videogames/name?=", videogameHandlerByNameQuery);
-
-router.post("/videogames", videogamePostHandler);
+router.post("/videogames", gamePostHandler);
 
 router.get("/genres", genreHandler);
+
 module.exports = router;
