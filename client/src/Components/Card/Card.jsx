47218937e28a835f4c4c85e249918493d6c7cb42
tranceblastr <brian.andrais@gmail.com>
2023-06-19 02:49:22 -0300

    Card y CardsContainer funcionales con array hardcodeado, estilos minimos

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,9 +1,19 @@
-const Card = (props) =>{
+import style from "./Card.module.css"
+
+
+const Card = ({ image, name, genres, rating }) => {
   return (
-    <div>
-      <h1>Una Card</h1>
-      
+    <div className={style.card}>
+      <img src={image} alt="Unavailable" />
+      <h3>{name}</h3>
+      <p>Genre: </p>
+      <p> {genres}</p>
+      <div className={style.rating}>
+        <p>Rating:</p>
+        <p>{rating}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default Card
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import CardsContainer from "../../Components/CardsContainer/CardsContainer";
 const Home =()=>{
   return (
     <div>
+      <h1>Vista de home</h1>
       <CardsContainer/>
    </div>
   )
